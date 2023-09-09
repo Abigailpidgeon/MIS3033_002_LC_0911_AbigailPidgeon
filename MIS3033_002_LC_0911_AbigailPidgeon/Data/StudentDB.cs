4827a1d51f4c06eddb95251af59dfab2200ef747
Abby Pidgeon <Abigail.L.Pidgeon-1@ou.edu>
2023-09-09 10:37:26 -0500

    Add project files.

---
+++ MIS3033_002_LC_0911_AbigailPidgeon/Data/StudentDB.cs
@@ -0,0 +1,15 @@
+﻿using a;
+using Microsoft.EntityFrameworkCore;
+
+namespace b
+{
+    public class StudentDB:DbContext
+    {
+        public DbSet<Student> StudentsTbl { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsbuilder)
+        {
+
+            optionsbuilder.UseSqlite("Data Source=Stu.db");
+        }
+    }
+}
---
+++ MIS3033_002_LC_0911_AbigailPidgeon/Program.cs
@@ -0,0 +1,18 @@
+﻿// MIS 3033 002
+// September 11 2023
+// Abigail Pidgeon
+using b;
+using a;
+
+Console.WriteLine("Entity Famework");
+
+StudentDB studb;// complex
+studb = new StudentDB();// we connect to database
+
+Student stu;
+stu = new Student() { Id="s126",Name="Abigail Pidgeon",Age=21};
+//stu.name = "Abigail Pidgeon"
+
+studb.StudentsTbl.Add(stu);//studb.studentstbl in the computer memoery
+
+studb.SaveChanges();
