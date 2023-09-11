585c6324fef84ec93f2cdf2ee2b390333bfb20dd
Abby Pidgeon <Abigail.L.Pidgeon-1@ou.edu>
2023-09-11 11:43:10 -0500

    finish lecture codes

--- MIS3033_002_LC_0911_AbigailPidgeon/Models/Student.cs
+++ MIS3033_002_LC_0911_AbigailPidgeon/Models/Student.cs
@@ -5,9 +5,17 @@ namespace a
     public class Student
     {
         [Key]
-        public string Id { get; set; }
+        public string Id { get; set; }//member field
         public string Name { get; set; }
         public int Age { get; set; }
+
+        public static string Instructor { get; set; } = "Xinglong Ju";//static
+        // static is not going to write to the database 
+
+        public override string ToString()//member function
+        {
+            return string.Format($"ID:{Id},Name:{Name}");
+        }
 
     }
 }
--- MIS3033_002_LC_0911_AbigailPidgeon/Program.cs
+++ MIS3033_002_LC_0911_AbigailPidgeon/Program.cs
@@ -3,6 +3,8 @@
 // Abigail Pidgeon
 using b;
 using a;
+using System.Text.Json;
+using System.Xml.Linq;
 
 Console.WriteLine("Entity Famework");
 
@@ -13,6 +15,60 @@ Student stu;
 stu = new Student() { Id="s126",Name="Abigail Pidgeon",Age=21};
 //stu.name = "Abigail Pidgeon"
 
-studb.StudentsTbl.Add(stu);//studb.studentstbl in the computer memoery
+//studb.StudentsTbl.Add(stu);//studb.studentstbl in the computer memoery
+//studb.SaveChanges();
 
-studb.SaveChanges();
+// static
+Student stu2;//complex
+stu2 = new Student() { Id = "S1", Name = "Dolly Parton", Age = 67 };
+Console.WriteLine(Student.Instructor);
+
+// anonymos class type
+var r1 = new { Id = "s2", Name = "Travis Kelce" };
+Console.WriteLine(r1.Name);
+
+// file JSON
+string filepath = "data.txt";
+File.WriteAllText(filepath, "200");
+
+// {} []
+// {}, holds one record
+// []: collection, holds multiple records but can be null
+
+string outMesStr;
+
+var opt = new JsonSerializerOptions();
+opt.WriteIndented = true;// member field 
+
+outMesStr = JsonSerializer.Serialize(stu2,opt);//static //function overload
+Console.WriteLine(outMesStr);
+File.WriteAllText(filepath, outMesStr);
+
+//[]
+var r2 = studb.StudentsTbl.Take(1);//r2 collection
+outMesStr = JsonSerializer.Serialize(r2, opt);//static //function overload
+Console.WriteLine(outMesStr);
+File.WriteAllText(filepath, outMesStr);
+
+// delegate, lambda expression
+// lambda expression: is a way to write a function. it is a shortcut to write said function.
+// arrow function
+
+int age1;// int datatype, age1: variable 
+int age2;// 
+
+Student stu3;
+stu3 = new Student() { Id = "S2", Name = "Jason Kelece", Age = 48 };
+//Console.WriteLine(stu3.ToString());
+
+bool CheckAge(Student s)
+{
+    return s.Age == 21;
+}
+
+bool CheckID(Student s)
+{
+    return s.Id == "s126";
+}
+
+Console.WriteLine(CheckAge(stu));
